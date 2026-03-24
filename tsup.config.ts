9de41c40cebda8e565ad9c9c6f7441a9b6f9abd6
Walter Robinson <me@walterr.cc>
2026-03-23 19:09:21 -0700

    Fix deployment

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"], // Build for commonJS and ESmodules
-//   dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
+  dts: true, // Generate declaration file (.d.ts)
+  splitting: true,
 //   sourcemap: true,
   clean: true,
 
